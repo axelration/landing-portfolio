e7831bcfbeed98a8b89324006262670c363bfed0
Axelration <raxeladam1@gmail.com>
2025-09-14 18:32:49 +1000

    Updates

---
+++ _next/static/UkSyGmTbJWlAaMEv6l_Rp/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/7aede8f8705c29f4.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/landing-portfolio//_next/:path+",
+        "destination": "/landing-portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
